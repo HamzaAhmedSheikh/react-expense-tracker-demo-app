672d3708a42914680052cf870a1e0c3bd269c169
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-17 04:53:29 +0500

    add Balance.js and modified src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header'
+import { Header } from './Components/Header'
+import { Balance } from './Components/Balance'
 
 function App() {
   return (
-    <div className="App">
-      <Header />      
+    <div>
+      <Header />   
+     <div className='container'>
+       <Balance />
+     </div>    
     </div>
   );
 }
