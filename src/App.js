695796be7cd164617ccb1f76a7073bc124e7c29c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-17 05:02:19 +0500

    add IncomeExpenses.js in src/App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Header } from './Components/Header'
 import { Balance } from './Components/Balance'
+import { IncomeExpenses } from './Components/IncomeExpenses'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />   
      <div className='container'>
        <Balance />
+       <IncomeExpenses />
      </div>    
     </div>
   );
